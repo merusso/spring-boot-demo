244f67826df0fae0a10412cb67ac8423b7d8ded5
Michael Russo <merusso@gmail.com>
2017-10-11 21:04:48 -0500

    Add initial files from Spring Boot Initializer.

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @ResponseBody
+    @RequestMapping("/")
+    public String home() {
+        logger.info("home");
+        return "Hello, world!";
+    }
+}
